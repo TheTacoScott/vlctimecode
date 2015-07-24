194501f9d94cb979e07f31468d457ca6c357266b
Scott Powers <scott.powers@untsystem.edu>
2015-07-24 13:17:08 -0500

    added xdotool foo to make mouse loc get happen

--- vlcthing.py
+++ vlcthing.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+import subprocess
 import requests,math
 from bs4 import BeautifulSoup
-import sys
+import sys,re,time
 try:
-  r = requests.get('http://localhost:8080/requests/status.xml', auth=('', 'aaaa'))
+  r = requests.get('http://localhost:9999/requests/status.xml', auth=('', 'aaaa'))
   soup = BeautifulSoup(r.text)
   length = float(soup.length.string)
   percent = float(soup.position.string)
@@ -18,3 +20,34 @@ mils = s
 mils = int(math.floor((mils - s) * 1000))
 
 print "'{0:02d}:{1:02d}:{2:02d}.{3:03d}'".format(h,m,s,mils)
+
+child = subprocess.Popen("xdotool getmouselocation --shell", shell=True, stdout=subprocess.PIPE)
+exec(child.stdout.read())
+mousex = X
+mousey = Y
+
+
+child = subprocess.Popen("xdotool search --class vlc getwindowgeometry --shell", shell=True, stdout=subprocess.PIPE)
+exec(child.stdout.read())
+border = 5
+top_menu = 19
+bottom_menu = 48
+
+vlcx = X
+vlcy = Y
+vlcw = WIDTH
+vlch = HEIGHT
+
+left = vlcx + border
+top = vlcy + border + top_menu
+
+right = left + vlcw - 1
+bottom = top + vlch - border - bottom_menu - top_menu
+
+#subprocess.Popen("xdotool mousemove {0} {1}".format(left,top), shell=True, stdout=subprocess.PIPE)
+#time.sleep(1)
+#subprocess.Popen("xdotool mousemove {0} {1}".format(right,bottom), shell=True, stdout=subprocess.PIPE)
+xp = round(float(mousex - left)/float(right-left),2)
+yp = round(float(mousey - top)/float(bottom-top),2)
+print xp,yp
+
