f9119ec7e553ec33604d3df9e2879072122d3dca
Scott Powers <scott.powers@untsystem.edu>
2015-07-24 14:06:43 -0500

    zenity scale foo

--- vlcthing.py
+++ vlcthing.py
@@ -3,6 +3,11 @@ import subprocess
 import requests,math
 from bs4 import BeautifulSoup
 import sys,re,time
+
+border = 5
+top_menu = 19
+bottom_menu = 48
+
 try:
   r = requests.get('http://localhost:9999/requests/status.xml', auth=('', 'aaaa'))
   soup = BeautifulSoup(r.text)
@@ -19,7 +24,7 @@ mils = s
 (h,m,s) = (int(math.floor(h)), int(math.floor(m)), int(math.floor(s)))
 mils = int(math.floor((mils - s) * 1000))
 
-print "'{0:02d}:{1:02d}:{2:02d}.{3:03d}'".format(h,m,s,mils)
+timecode = "'{0:02d}:{1:02d}:{2:02d}.{3:03d}'".format(h,m,s,mils)
 
 child = subprocess.Popen("xdotool getmouselocation --shell", shell=True, stdout=subprocess.PIPE)
 exec(child.stdout.read())
@@ -29,14 +34,14 @@ mousey = Y
 
 child = subprocess.Popen("xdotool search --class vlc getwindowgeometry --shell", shell=True, stdout=subprocess.PIPE)
 exec(child.stdout.read())
-border = 5
-top_menu = 19
-bottom_menu = 48
-
 vlcx = X
 vlcy = Y
 vlcw = WIDTH
 vlch = HEIGHT
+
+child = subprocess.Popen("zenity --scale --text='Zoom' --min-value=0 --max-value=100 --step=5 --value=100", shell=True, stdout=subprocess.PIPE)
+zoom = float(child.stdout.read()) * 0.01
+
 
 left = vlcx + border
 top = vlcy + border + top_menu
@@ -49,5 +54,5 @@ bottom = top + vlch - border - bottom_menu - top_menu
 #subprocess.Popen("xdotool mousemove {0} {1}".format(right,bottom), shell=True, stdout=subprocess.PIPE)
 xp = round(float(mousex - left)/float(right-left),2)
 yp = round(float(mousey - top)/float(bottom-top),2)
-print xp,yp
+print zoom,xp,yp,timecode
 
